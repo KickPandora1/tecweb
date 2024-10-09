a974ad891a26d224d4e89aaf4af7f332d461b404
KickPandora1 <144202882+KickPandora1@users.noreply.github.com>
2024-10-08 19:19:46 -0600

    End p08

--- practicas/p08/set_producto_v2.php
+++ practicas/p08/set_producto_v2.php
@@ -5,7 +5,7 @@ $modelo = isset($_POST["modelo"])? $_POST["modelo"] : 'modelo_producto';
 $precio = isset($_POST["precio"])? $_POST["precio"] : 'precio_producto';
 $detalles = isset($_POST["detalles"])? $_POST["detalles"] : 'detalles_producto';
 $unidades = isset($_POST["unidades"])? $_POST["unidades"] : 'unidades_producto';
-$url = isset($_POST["urlImage"])? $_POST["urlImage"] : 'url_imagen';
+$imagen = isset($_POST["imagen"])? $_POST["imagen"] : 'imagen';
 /** SE CREA EL OBJETO DE CONEXION */
 @$link = new mysqli('localhost', 'root', 'Ubi131418', 'marketzone');	
 
@@ -18,17 +18,26 @@ if ($link->connect_errno)
 
 /** Crear una tabla que no devuelve un conjunto de resultados */
 $sql_check = "SELECT * FROM productos WHERE nombre = '{$nombre}' AND marca = '{$marca}' AND modelo = '{$modelo}'";
-$sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$url}')";
 $result_check = $link->query($sql_check);
 
-if ($result_check->num_rows > 0) 
-    {
-        // Si ya existe el producto, mostrar un mensaje de error
-        echo "<p>El producto ya existe en la base de datos.</p>";
+if ($result_check->num_rows == 0) {
+    /* Si no existe, se inserta el producto 
+    
+    $sql_insert = "INSERT INTO productos (nombre, marca, modelo, precio, unidades, eliminado) 
+    VALUES ('{$nombre}', '{$marca}', '{$modelo}', {$precio}, {$unidades}, 0)";
+    */
+    //Nueva query sin incluir las columnas ide ni eliminado
+    $sql_insert = "INSERT INTO productos (nombre, marca, modelo, precio, unidades) 
+                VALUES ('{$nombre}', '{$marca}', '{$modelo}', {$precio}, {$unidades})";
+
+
+    if ($link->query($sql_insert)) {
+        echo 'Producto insertado con éxito con ID: ' . $link->insert_id;
+    } else {
+        echo 'Error al insertar el producto: ' . $link->error;
     }
-else
-{
-    echo 'Producto insertado con ID: '.$link->insert_id;
+} else {
+    echo "<p>El producto ya existe en la base de datos.</p>";
 }
 
 
